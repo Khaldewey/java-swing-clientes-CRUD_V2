6955fb6edf962ba9829e526d5ff68c6f38aadd8b
Israel <israel_alves77@hotmail.com>
2024-09-04 19:52:37 -0300

    first commit

---
+++ src/swing/customer/crud/dao/IClienteDAO.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package swing.customer.crud.dao;
+
+import java.util.Collection;
+import swing.customer.crud.domain.Cliente;
+
+
+
+/**
+ * @author rodrigo.pires
+ */
+public interface IClienteDAO {
+
+    public Boolean cadastrar(Cliente cliente);
+
+    public void excluir(Long cpf);
+
+    public void alterar(Cliente cliente);
+
+    public Cliente consultar(Long cpf);
+
+    public Collection<Cliente> buscarTodos();
+}
